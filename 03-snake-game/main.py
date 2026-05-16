b3b22268f9ff68f77e73eb980d4d15995857a3d0
Michal <michalmechlinski1b1@gmail.com>
2026-05-16 18:43:59 +0200

    Initial Snake game commit

---
+++ 03-snake-game/main.py
@@ -0,0 +1,18 @@
+import customtkinter as ctk
+from settings import *
+
+class Game(ctk.CTk):
+	def __init__(self):
+		super().__init__()
+		self.title('Snake')
+		self.geometry(f'{WINDOW_SIZE[0]}x{WINDOW_SIZE[1]}')
+
+		# layout
+		self.columnconfigure(list(range(FIELDS[0])),weight=1,uniform='a')
+		self.rowconfigure(list(range(FIELDS[1])),weight=1,uniform='a')
+		
+
+		# run
+		self.mainloop()
+
+Game()
