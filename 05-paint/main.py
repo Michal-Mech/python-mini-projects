a270e3ce6bf580b9f669af066dc53c7538385b16
Michal <michalmechlinski1b1@gmail.com>
2026-06-01 23:19:35 +0200

    paint app initial commit

---
+++ 05-paint/main.py
@@ -0,0 +1,22 @@
+import customtkinter as ctk
+from drawing_area import DrawingArea
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('800x600')
+        self.title(' ')
+        self.iconbitmap('images/empty.ico')
+        ctk.set_appearance_mode('light')
+
+        #widgets
+        DrawingArea(self)
+
+
+    def run(self):
+        self.mainloop()
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
