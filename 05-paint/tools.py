a8d2104d50bc6dd7ef64783d88cbf9572f1a9e26
Michal <michalmechlinski1b1@gmail.com>
2026-06-09 16:36:42 +0200

    add color sliders

--- 05-paint/tools.py
+++ 05-paint/tools.py
@@ -20,12 +20,48 @@ class ToolPanel(ctk.CTkToplevel):
         self.rowconfigure((2,3),weight=1,uniform='a')
 
         # widgets
+        ColorSliderPanel(self,color_string)
         BrushSizeSlider(self,brush_float)
         ColorPanel(self,color_string)
 
 
     def close_app(self):
         self.parent.quit()
+
+class ColorSliderPanel(ctk.CTkFrame):
+    def __init__(self,parent,color_string):
+        super().__init__(master=parent)
+        self.grid(row=0,column=0,sticky='nsew',pady = 5, padx = 5)
+
+        # data
+        self.color_string = color_string
+        self.r_int = ctk.IntVar(value = self.color_string.get()[0])
+        self.g_int = ctk.IntVar(value = self.color_string.get()[1])
+        self.b_int = ctk.IntVar(value = self.color_string.get()[2])
+        self.color_string.trace('w',self.set_color)
+
+        # layout
+        self.rowconfigure((0,1,2),weight=1,uniform='a')
+        self.columnconfigure(0,weight=1,uniform='a')
+
+        #wwidgets
+        ctk.CTkSlider(self, from_= 0, to= 15, number_of_steps= 16, command= lambda value: self.set_single_color('r',value),variable= self.r_int,button_color=SLIDER_RED,button_hover_color=SLIDER_RED).grid(row=0,column=0,padx = 5,pady = 5)
+        ctk.CTkSlider(self, from_= 0, to= 15, number_of_steps= 16, command= lambda value: self.set_single_color('g',value),variable=self.g_int,button_color=SLIDER_GREEN,button_hover_color=SLIDER_GREEN).grid(row=1,column=0,padx = 5,pady = 5)
+        ctk.CTkSlider(self, from_= 0, to= 15, number_of_steps= 16, command= lambda value: self.set_single_color('b',value),variable=self.b_int,button_color=SLIDER_BLUE,button_hover_color=SLIDER_BLUE).grid(row=2,column=0,padx = 5,pady = 5)
+
+    def set_single_color(self,color,value):
+        current_color_list = list(self.color_string.get())
+
+        match color:
+            case 'r': current_color_list[0] = COLOR_RANGE[int(value)]
+            case 'g': current_color_list[1] = COLOR_RANGE[int(value)]
+            case 'b': current_color_list[2] = COLOR_RANGE[int(value)]
+        self.color_string.set(f'{"".join(current_color_list)}')
+
+    def set_color(self,*args):
+        self.r_int.set(COLOR_RANGE.index(self.color_string.get()[0]))
+        self.g_int.set(COLOR_RANGE.index(self.color_string.get()[1]))
+        self.b_int.set(COLOR_RANGE.index(self.color_string.get()[2]))
 
 class ColorPanel(ctk.CTkFrame):
     def __init__(self,parent,color_string):
@@ -46,13 +82,13 @@ class ColorPanel(ctk.CTkFrame):
     def pick_color(self, color):
         self.color_string.set(color)
 
-
 class ColorFieldButton(ctk.CTkButton):
     def __init__(self,parent,row,col,color,pick_color):
         super().__init__(
             master=parent,
             text = '',
             fg_color= f'#{color}',
+            hover_color= f'#{color}',
             corner_radius=1,
             command= self.click_handler
         )
@@ -63,7 +99,6 @@ class ColorFieldButton(ctk.CTkButton):
 
     def click_handler(self):
         self.pick_color(self.color)
-
 
 class BrushSizeSlider(ctk.CTkFrame):
     def __init__(self,parent,brush_float):
